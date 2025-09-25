5c8889b77db42ad0ff510fb67595a768f6e18211
Dmitry Bugaev <bugaev1404g@mail.ru>
2025-09-25 15:52:32 +0300

    Добавил пару новых автотестов

--- Lesson_01.py
+++ Lesson_01.py
@@ -1,6 +1,43 @@
 from selene import browser, be, have
 
-#Другой комментарий
-browser.open('https://google.com')
-browser.element('[class="search3__input mini-suggest__input"]').should(be.blank).type('qa.guru').press_enter()
-browser.element('html').should(have.text('About this page'))
+
+# browser.open('https://demoqa.com/text-box')
+# browser.element('[id="userName"]').type('Bugaev Dmitry') #.should(be.blank) .press_enter()
+# browser.element('[id="userEmail"]').type('bugaev1404g@mail.ru')
+# browser.element('[id="submit"]').press_enter()
+# browser.element('[id="name"]').should(have.text('Name:Bugaev Dmitry'))
+# browser.element('[id="email"]').should(have.text('Email:bugaev1404g@mail.ru'))
+
+# browser.open('https://ya.ru')
+# browser.element('[id="text"]').type('Прогноз погоды Санкт-Петербург').press_enter()
+# browser.element('html').should(have.text('Погода в Санкт-Петербурге'))
+
+from selene import browser, have
+
+# def test_success_login():
+#     browser.open('https://niffler.qa.guru')
+#     browser.element('[id="username"]').type('stas')
+#     browser.element('[id="password"]').type('12345')
+#     browser.element('[id="login-button"]').click()
+#
+#     browser.element('[id="spendings"]').should(have.text('History of Spendings'))
+#     browser.quit()
+#
+# def test_success_login_with_press_enter():
+#     browser.open('https://niffler.qa.guru')
+#     browser.element('[id="username"]').type('stas')
+#     browser.element('[id="password"]').type('12345').press_enter()
+#
+#     browser.element('[id="spendings"]').should(have.text('History of Spendings'))
+#     browser.quit()
+#
+# def test_wrong_credentials():
+#     browser.open('https://niffler.qa.guru')
+#     browser.element('[id="username"]').type('stas')
+#     browser.element('[id="password"]').type('12345sgsrgwr').press_enter()
+#
+#     browser.element('[class="form__error"]').should(have.text('Bad credentials'))
+#     browser.quit()
+#
+# test_success_login()
+
